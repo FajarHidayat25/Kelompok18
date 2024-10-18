4b2d19123540690a31e428c7cb1da5c660dbabe5
FajarHidayat25 <129923915+FajarHidayat25@users.noreply.github.com>
2024-10-18 16:32:34 +0700

    Add files via upload

---
+++ homejs.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    alert("Selamat datang!");
+
+    $(window).on("scroll", function() {
+        if ($(this).scrollTop() > 200) {
+            $("#scroll-top").fadeIn(200);  
+        } else {
+            $("#scroll-top").fadeOut(200); 
+        }
+    });
+
+    $("#scroll-top").on("click", function(event) {
+        event.preventDefault();
+        $("html, body").stop().animate({scrollTop: 0}, 50, 'linear'); 
+    });
+});
